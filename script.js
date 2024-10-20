240576765e3d855f8ef169d3bb4e7ef3fb5b6c0b
Beatriz Karoline <111443798+SouBeatrizKaroline@users.noreply.github.com>
2024-10-20 12:29:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.getElementById('welcomeButton').addEventListener('click', function() {
-    alert('Bem-vindo ao Vê Só! Estamos felizes em tê-lo aqui.');
+    alert('Bem-vindo ao Vê Só! Estamos felizes em tê-lo aqui. Nosso App estará disponivel em breve!');
 });
